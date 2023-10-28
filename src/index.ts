1925323e525cc7b96be7d686054508ca9be053ff
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-28 23:31:02 +0530

    added chnages

--- src/index.ts
+++ src/index.ts
@@ -25,147 +25,6 @@ if (ZapparThree.browserIncompatible()) {
   throw new Error("Unsupported browser");
 }
 
-function startFirecrackerAnimation(rendererCanvas: HTMLCanvasElement) {
-  const c = rendererCanvas;
-  if (c) {
-    const ctx = c.getContext("2d");
-
-    let cwidth: any, cheight: any;
-    const shells: any = [];
-    const pass: any = [];
-
-    const colors = [
-      "#FF5252",
-      "#FF4081",
-      "#E040FB",
-      "#7C4DFF",
-      "#536DFE",
-      "#448AFF",
-      "#40C4FF",
-      "#18FFFF",
-      "#64FFDA",
-      "#69F0AE",
-      "#B2FF59",
-      "#EEFF41",
-      "#FFFF00",
-      "#FFD740",
-      "#FFAB40",
-      "#FF6E40",
-    ];
-
-    window.onresize = function () {
-      reset();
-    };
-    reset();
-
-    function reset() {
-      cwidth = c.width;
-      cheight = c.height;
-    }
-
-    function newShell() {
-      var left: any = Math.random() > 0.5;
-      var shell: any = {};
-      shell.x = 1 * left;
-      shell.y = 1;
-      shell.xoff = (0.01 + Math.random() * 0.007) * (left ? 1 : -1);
-      shell.yoff = 0.01 + Math.random() * 0.007;
-      shell.size = Math.random() * 6 + 3;
-      shell.color = colors[Math.floor(Math.random() * colors.length)];
-
-      shells.push(shell);
-    }
-
-    function newPass(shell: any) {
-      const pasCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
-
-      for (let i = 0; i < pasCount; i++) {
-        var pas: any = {};
-        pas.x = shell.x * cwidth;
-        pas.y = shell.y * cheight;
-
-        const a = Math.random() * 4;
-        const s = Math.random() * 10;
-
-        pas.xoff = s * Math.sin((5 - a) * (Math.PI / 2));
-        pas.yoff = s * Math.sin(a * (Math.PI / 2));
-
-        pas.color = shell.color;
-        pas.size = Math.sqrt(shell.size);
-
-        if (pass.length < 1000) {
-          pass.push(pas);
-        }
-      }
-    }
-
-    var lastRun = 0;
-    Run();
-
-    function Run() {
-      let dt = 1;
-      if (lastRun != 0) {
-        dt = Math.min(50, performance.now() - lastRun);
-      }
-      lastRun = performance.now();
-      if (ctx) {
-        ctx.fillStyle = "rgba(0,0,0,0.25)";
-        ctx.fillRect(0, 0, cwidth, cheight);
-
-        if (shells.length < 10 && Math.random() > 0.96) {
-          newShell();
-        }
-
-        for (let ix in shells) {
-          var shell = shells[ix];
-
-          ctx.beginPath();
-          ctx.arc(
-            shell.x * cwidth,
-            shell.y * cheight,
-            shell.size,
-            0,
-            2 * Math.PI
-          );
-          ctx.fillStyle = shell.color;
-          ctx.fill();
-
-          shell.x -= shell.xoff;
-          shell.y -= shell.yoff;
-          shell.xoff -= shell.xoff * dt * 0.001;
-          shell.yoff -= (shell.yoff + 0.2) * dt * 0.00005;
-
-          if (shell.yoff < -0.005) {
-            newPass(shell);
-            shells.splice(ix, 1);
-          }
-        }
-
-        for (let ix in pass) {
-          var pas = pass[ix];
-
-          ctx.beginPath();
-          ctx.arc(pas.x, pas.y, pas.size, 0, 2 * Math.PI);
-          ctx.fillStyle = pas.color;
-          ctx.fill();
-
-          pas.x -= pas.xoff;
-          pas.y -= pas.yoff;
-          pas.xoff -= pas.xoff * dt * 0.001;
-          pas.yoff -= (pas.yoff + 5) * dt * 0.0005;
-          pas.size -= dt * 0.002 * Math.random();
-
-          if (pas.y > cheight || pas.y < -50 || pas.size <= 0) {
-            pass.splice(ix, 1);
-          }
-        }
-      }
-
-      requestAnimationFrame(Run);
-    }
-  }
-}
-
 // ZapparThree provides a LoadingManager that shows a progress bar while
 // the assets are downloaded. You can use this if it's helpful, or use
 // your own loading UI - it's up to you :-)
@@ -233,6 +92,56 @@ gltfLoader.load(
   }
 );
 
+//==================FIRE CRACKERs========================
+
+function getRandomHexColorWithAlpha() {
+  // Generate a random integer between 0 and 0xffffff (16777215 in decimal)
+  const randomColor = Math.floor(Math.random() * 0xffffff);
+  // Convert the integer to a hexadecimal string with '0x' prefix
+  const hexColor = `0x${randomColor.toString(16)}`;
+  // Add the '1' at the end
+  return `${hexColor}1`;
+}
+
+// Particle system parameters
+const particleCount = 1000; // Adjust the number of particles as desired
+const particleSize = 0.1; // Adjust the size of the particles
+const particleColor = getRandomHexColorWithAlpha(); // Adjust the color of the particles
+
+// Create the particle system
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesMaterial = new THREE.PointsMaterial({
+  size: particleSize,
+  color: particleColor,
+});
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+// Generate random particle positions and colors
+const positions = [];
+const colors = [];
+
+for (let i = 0; i < particleCount; i++) {
+  const x = (Math.random() - 0.5) * 10;
+  const y = (Math.random() - 0.5) * 10;
+  const z = (Math.random() - 0.5) * 10;
+  positions.push(x, y, z);
+
+  const r = Math.random();
+  const g = Math.random();
+  const b = Math.random();
+  colors.push(r, g, b);
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(positions, 3)
+);
+particlesGeometry.setAttribute(
+  "color",
+  new THREE.Float32BufferAttribute(colors, 3)
+);
+
 // Let's add some lighting, first a directional light above the model pointing down
 const directionalLight = new THREE.DirectionalLight("white", 0.8);
 directionalLight.position.set(0, 5, 0);
@@ -253,9 +162,6 @@ placeButton.addEventListener("click", () => {
   hasPlaced = true;
   mymodel.visible = true;
   placeButton.remove();
-
-  // Start the firecracker animation
-  startFirecrackerAnimation(canvas);
 });
 
 // Get a reference to the 'Snapshot' button so we can attach a 'click' listener
@@ -321,6 +227,16 @@ function render(): void {
     // to be directly in front of the user
     instantTrackerGroup.setAnchorPoseFromCameraOffset(0, 0, -5);
   }
+
+  // Update particle positions or properties here
+  const positions: any = particlesGeometry.getAttribute("position").array;
+  for (let i = 0; i < positions.length; i += 3) {
+    positions[i + 1] += Math.random() * 0.01; // Move particles upward
+    if (positions[i + 1] > 5) {
+      positions[i + 1] = -5; // Reset particles' Y position when they go beyond the screen
+    }
+  }
+  particlesGeometry.getAttribute("position").needsUpdate = true;
 
   // The Zappar camera must have updateFrame called every frame
   camera.updateFrame(renderer);
